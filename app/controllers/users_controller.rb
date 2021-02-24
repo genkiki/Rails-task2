021fc248794a572ccec99885eb72a3e19e61fa41
genki.konda <gegege0126@gmail.com>
2021-02-24 09:25:53 +0900

    login function is under devolopment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def create
+    puts 'start create !!!!!!!'
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+
+    check_pass = params[:password] == params[:password_confirmation]
+    puts "check_pass:#{check_pass}"
+    if check_pass && @user.save
+      puts "create success!!!!!!!!!!"
+      redirect_to "/"
+    else
+      puts "create failed !!!!!!!!"
+      redirect_to '/users/new'
+    end
   end
 end
