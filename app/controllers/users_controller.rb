ec9f31ce1256693682bda9582c5b859f02da8647
genki.konda <gegege0126@gmail.com>
2021-03-03 10:44:25 +0900

    Create login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,44 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
     puts 'start create !!!!!!!'
-    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    new_user = User.new(name: params[:name],
+                      email: params[:email],
+                      password: params[:password],
+                      password_confirmation: params[:password_confirmation] )
 
-    check_pass = params[:password] == params[:password_confirmation]
-    puts "check_pass:#{check_pass}"
-    if check_pass && @user.save
+    if new_user.save
       puts "create success!!!!!!!!!!"
+      session[:user_id] = new_user.id
+      flash[:notice] = "ユーザー登録成功しました"
       redirect_to "/"
     else
       puts "create failed !!!!!!!!"
-      redirect_to '/users/new'
+      flash[:notice] = "ユーザー登録失敗しました"
+      render new_user
+    end
+  end
+
+  def sign_in
+  end
+
+  def login
+    login_user = User.find_by(email: params[:email])
+    puts "params[:email]:#{params[:email]}"
+    puts "params[:password]:#{params[:password]}"
+    puts "email:#{login_user.email}"
+    puts "password:#{login_user.password}"
+    if login_user.authenticate(params[:password])
+      session[:user_id] = login_user.id
+      flash[:notice] = "ログインしました"
+      puts "login success!!!!!!!!!!"
+      redirect_to "/"
+    else
+      flash[:notice] = "ログイン失敗しました"
+      puts "login failed!!!!!!!!!!"
+      render "/users/sign_in"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'top#index'
   get '/users/sign_in', to: 'users#sign_in'
+  post '/users/sign_in', to: 'users#login'
 
   resources :hotels
   resources :users
