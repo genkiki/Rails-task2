86f7561f15dfdf3853bdb9d928bac9314bcc7a5d
genki.konda <gegege0126@gmail.com>
2021-03-10 19:43:13 +0900

    Create hotel regist page

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -6,4 +6,18 @@ class HotelsController < ApplicationController
   def show
     @hotel = Hotel.find_by(id: params[:id])
   end
+
+  def create
+    new_hotel = Hotel.new(user_id: session[:user_id],
+                          name: params[:name],
+                          place: params[:place],
+                          price: params[:price],
+                          discription: params[:discription])
+
+    if new_hotel.save
+      redirect_to hotel_path(new_hotel.id)
+    else
+      render new_hotel_path
+    end
+  end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,6 @@
 class Hotel < ApplicationRecord
     belongs_to :user
     has_many :reserve_infos
+
+    mount_uploader :image_icon, ImgUploader
 end
